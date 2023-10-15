d7daa354e0aa760d31bcc502c1de3b28161e0415
hassan alabdulal <hassan.alabdulaaal@gmail.com>
2023-10-15 18:36:16 +0300

    JS 3 Exercise 3

--- screensaver.js
+++ screensaver.js
@@ -1,75 +1,43 @@
-
 let timeout;
-let scene, camera, renderer;
-let shapes = [];
+let scene, camera, renderer, geometry, material, mesh;
 
 function init() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('screensaverCanvas') });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById('screensaver').appendChild(renderer.domElement);
+    document.body.appendChild(renderer.domElement);
 
-    // OrbitControls allow a camera to orbit around the object
-    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    geometry = new THREE.BoxGeometry();
+    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
-    // Geometrical shapes
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-
-    const geometries = [
-        new THREE.TorusGeometry(5, 3, 16, 100),
-        new THREE.SphereGeometry(4, 32, 32),
-        new THREE.BoxGeometry(5, 5, 5)
-    ];
-
-    geometries.forEach(geometry => {
-        const shape = new THREE.Mesh(geometry, material);
-        shapes.push(shape);
-        scene.add(shape);
-    });
-
-    camera.position.z = 15;
+    camera.position.z = 5;
 }
 
 function animate() {
     requestAnimationFrame(animate);
-
-    shapes.forEach(shape => {
-        shape.rotation.x += 0.005;
-        shape.rotation.y += 0.005;
-    });
-
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
-function startScreensaver() {
-    document.getElementById('screensaver').style.display = 'block';
-    init();
+document.addEventListener('mousemove', resetTimer, false);
+document.addEventListener('mousedown', resetTimer, false);
+document.addEventListener('keypress', resetTimer, false);
+document.addEventListener('touchmove', resetTimer, false);
+
+function resetTimer() {
+    clearTimeout(timeout);
+    document.getElementById('screensaverCanvas').style.display = 'none';
+    timeout = setTimeout(showScreensaver, 60000);
+}
+
+function showScreensaver() {
+    document.getElementById('screensaverCanvas').style.display = 'block';
     animate();
 }
 
-function stopScreensaver() {
-    document.getElementById('screensaver').style.display = 'none';
-    if (renderer) {
-        renderer.domElement.remove();
-        renderer = null;
-        scene = null;
-        camera = null;
-        shapes = [];
-    }
-}
-
-document.addEventListener('mousemove', () => {
-    clearTimeout(timeout);
-
-    if (renderer) {
-        stopScreensaver();
-    }
-
-    timeout = setTimeout(() => {
-        startScreensaver();
-    }, 60000); // 1 minute of no activity
-});
-
-// Initial trigger to start the screensaver timeout
-document.dispatchEvent(new Event('mousemove'));
+init();
+resetTimer();
